8f75719939373e2fc2511eab4f5a3f1f1b01c09d
SenorYuen <adam.yuen@ucalgary.ca>
2024-06-30 19:35:15 -0600

    test commit for exercise 3.4.2

---
+++ src/Application.java
@@ -0,0 +1,21 @@
+/**
+ * <h1>Application.java</h1>
+ * <P>
+ * This is an introduction to Eclipse
+ * Lab 1, Exercise 2
+ * </p>
+ * <p>
+ * <b>Submission Date:</b> July 4th, 2024</p>
+ * @author Adam Yuen
+ * @Version 1.0
+ * 
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(" Hello, World!");
+		// Change for exercise 3.4.2 - this comment
+
+	}
+
+}
